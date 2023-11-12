a7cf6e79f288b124d598e15f016415b49905c302
Hedy-Khoshnaw <74620899+Hedy-Khoshnaw@users.noreply.github.com>
2023-11-12 19:04:14 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
 module.exports = {
-    token: process.env.TOKEN || "TOKEN_HERE",  // your bot token
+    token: process.env.TOKEN || "ODM0ODc0NTg2MjAxNDU2NjQw.GdMxTK.y90bg3ol8BrmMdTQVZomJqhzJsO_3R9n1fekds",  // your bot token
     prefix:  process.env.PREFIX || "!", // bot prefix
-    ownerID: process.env.OWNERID || "801478547893387345", //your discord id
+    ownerID: process.env.OWNERID || "700971576367251488", //your discord id
     mongourl: process.env.MONGO_URI || "MONGO_URI", // MongoDb URL
     embedColor: process.env.COLOR || 0x303236, // embed colour
     logs: process.env.LOGS || "", // channel id for guild create and delete logs
