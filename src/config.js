c7c77eed2a912ba7dc3e3c96637d7061fe3a0d49
Hedy-Khoshnaw <74620899+Hedy-Khoshnaw@users.noreply.github.com>
2023-11-19 13:25:01 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-    token: process.env.TOKEN || "ODM0ODc0NTg2MjAxNDU2NjQw.GKksra.SikT00wDNJp5-Q7mPbZSqZuIMQMUsa11X3hITQ",  // your bot token
+    token: process.env.TOKEN || "ODY4NTQ2NTIyOTQwMzEzNjMw.Ga6_-X.xkAa7AXljHjxPJbPj0gXvLQTZHSQflqbB_zXno",  // your bot token
     prefix:  process.env.PREFIX || "!", // bot prefix
     ownerID: process.env.OWNERID || "700971576367251488", //your discord id
     mongourl: process.env.MONGO_URI || "MONGO_URI", // MongoDb URL
